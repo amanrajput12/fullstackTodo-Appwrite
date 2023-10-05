82be51e97b93f716c3e8feb8b23289841ca79722
AMAN RAJPUT <rajputaman724857@gmail.com>
2023-10-05 12:23:11 +0530

    config update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+  proxy:{
+    '/api': 'https://todofullstack-backend-production.up.railway.app',
+    
+  }
+  },
   plugins: [react()],
 })
+
+
